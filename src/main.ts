e25523c2097b0c830f4414207d44416ad0490c0b
Thomas Jakemeyn <thomas.jakemeyn@realimpactanalytics.com>
2019-05-02 21:46:02 +0200

    Implemeting prototype

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import * as fs from 'fs';
 import * as glob from 'glob';
+import * as git from 'isomorphic-git';
 import { promisify } from 'util';
 import * as yaml from 'yaml';
 import { AppModule } from './app.module';
@@ -42,7 +43,47 @@ async function bootstrap() {
       graph.addNode(xbt);
     }
   });
+  //console.log(graph.sort());
 
-  console.log(graph.sort());
+  git.plugins.set('fs', fs);
+  const dir = '.';
+  const gitdir = dir;
+
+  // case 1: compare branch with reference
+  const ref = 'master';
+  const refCommit = await git.resolveRef({ dir, ref });
+  const headCommit = await git.resolveRef({ dir, ref: 'HEAD' });
+
+  // Include changes since the first commit after the common ancestor of both the reference commit and the HEAD
+  let commits = [];
+  let currentCommit = headCommit;
+  while(currentCommit !== refCommit
+    && !(await git.isDescendent({dir, oid: refCommit, ancestor: currentCommit, depth: '-1'}))) {
+    commits = await git.log({ dir, depth: 2, ref: currentCommit });
+    if (commits.length < 2) {
+      console.log(`No common ancestor found for HEAD and ${ref}!`);
+      process.exit(1);
+    }
+    currentCommit = commits[1].oid;
+  }
+  const firstCommit = commits.length > 0 ? commits[0] : null;
+
+  if (!firstCommit) {
+    console.log('No change detected');
+    process.exit(0);
+  }
+  console.log(`Include changes starting from commit ${firstCommit}`);
+
+  const trees = [
+    git.TREE({fs, gitdir, ref: firstCommit}),
+    git.TREE({fs, gitdir, ref: headCommit}),
+  ];
+  const filter = async ([first, last]: git.WalkerEntry) => {
+    await first.populateHash();
+    await last.populateHash();
+    return first.oid === last.oid;
+  };
+  const changes = await git.walkBeta1({trees, filter});
+  console.log(changes);
 }
 bootstrap();
