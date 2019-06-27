0339b245e3610440a5a8e63b0db285a673f2ac89
Thomas Jakemeyn <thomas.jakemeyn@realimpactanalytics.com>
2019-06-27 11:16:33 +0200

    Improved error handling

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,24 @@ import { AppService } from './app.service';
 import { CliModule } from './cli/cli.module';
 import { CliService } from './cli/cli.service';
 
+process
+  .on('uncaughtException', onUncaughtException)
+  .on('unhandledRejection', onUncaughtException)
+  .on('SIGTERM', onShutdownSignal)
+  .on('SIGINT', onShutdownSignal);
+
+  function onUncaughtException(exception) {
+    // tslint:disable-next-line: no-console
+    console.error(`Uncaught exception, exit(1): ${exception}`);
+    process.exit(1);
+  }
+  
+  function onShutdownSignal() {
+    // tslint:disable-next-line: no-console
+    console.info('Got shutdown signal, exit(0)');
+    process.exit(0);
+  }
+
 async function bootstrap() {
   const cliContext = await NestFactory.createApplicationContext(CliModule, { logger: false });
   const cliService = cliContext.get<CliService>(CliService);
